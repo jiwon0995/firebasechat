f356e2b8ef675c699df16b59c4190a696993b716
jiwon0995 <jiwon0995@gmail.com>
2021-12-03 22:38:13 +0900

    firebase 채팅구현

--- web-start/src/firebase-config.js
+++ web-start/src/firebase-config.js
@@ -8,12 +8,18 @@
  */
 const config = {
   /* TODO: ADD YOUR FIREBASE CONFIGURATION OBJECT HERE */
+  apiKey: "AIzaSyDPiQa9CwBSEa75li0VtFHJRlKxcyG_Op8",
+  authDomain: "chattest-4abb4.firebaseapp.com",
+  projectId: "chattest-4abb4",
+  storageBucket: "chattest-4abb4.appspot.com",
+  messagingSenderId: "293412805672",
+  appId: "1:293412805672:web:fa6cd043feb48bda90691d"
 };
 
 export function getFirebaseConfig() {
   if (!config || !config.apiKey) {
     throw new Error('No Firebase configuration object provided.' + '\n' +
-    'Add your web app\'s configuration object to firebase-config.js');
+      'Add your web app\'s configuration object to firebase-config.js');
   } else {
     return config;
   }
